4d5b271f0f74c5d49240f018f13697d7d00da75f
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2022-11-15 14:34:53 +0100

    Job04 - Mise en place d une boucle et d une condition if-elseif

--- Jour02/Job02/index.php
+++ Jour02/Job02/index.php
@@ -1,5 +1,9 @@
 <?php
 
+// Afficher tous les nombres de 0 à 1337 compris SAUF 26, 37, 88, 1111 et 3233 en
+// mettant un retour à la ligne entre chaque nombre (<br />).
+
+
 echo "Il manque certains nombres:";
 echo "<br>";
 for ($i = 0; $i <= 1337; $i++){
