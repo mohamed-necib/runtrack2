c5f8b4e57b9da7e5ef66a20c1aabdd62e3cc1fae
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2022-11-16 13:47:29 +0100

    Ajout Job04 - Mise en place d une boucle pour itérer dans une chaine de caractères et un compteur qui incrémente à chaque tour sur chacuns des caracteres puis on affiche le compteur

---
+++ Jour03/Job04/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// Créez une variable de type string nommée $str et affectez y le texte :
+// “Dans l'espace, personne ne vous entend crier”.
+// Créez un algorithme qui parcourt, compte et affiche le nombre de caractères présents
+// dans cette chaîne.
+
+$str = "Dans l'espace, personne ne vous entend crier";
+
+$nb=0;
+
+for($i=0; isset($str[$i]); $i++){
+     $nb++;
+};
+ 
+echo "\n $nb";
+
+?>
