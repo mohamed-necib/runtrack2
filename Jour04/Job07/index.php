b901032c9b9dc2726c66ec53ad729df1e4ad80b2
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2022-11-17 21:25:30 +0100

    Ajout Job07 - Mise en place d un formulaire de type $_GET avec deux champ text nommé largeur et hauteur et d un bouton submit - faire en sorte que lorsqu a la validation du formulaire on affiche une petite maison de la hauteur et de la largeur souhaitée

---
+++ Jour04/Job07/index.php
@@ -0,0 +1,78 @@
+<!-- Faire un formulaire avec deux <input> de type text ayant comme nom “largeur” et
+“hauteur” et un bouton submit.
+Vous devez créer un algorithme qui affiche, à la validation du formulaire,
+une maison telle que :
+Si on entre les valeurs : largeur =10 et hauteur = 5 dans les champs, la
+maison qui s’affiche sur la page doit ressembler à ceci :
+Si on entre les valeurs largeur = 20 et hauteur = 10 dans les champs,
+la maison qui s’affiche sur la page doit ressembler à ceci : -->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+
+<!-- Formulaire -->
+
+<form action="index.php" method="get" name="formulaire">
+
+<br/><input type="text" placeholder="largeur" name="largeur" value="">
+<input type="text" placeholder="hauteur" name="hauteur" value="">
+  
+<input type="submit" value="Envoyer">
+
+</form>
+</body>
+</html>
+
+
+
+<pre>
+<?php
+
+// Initialisation de la page
+
+if ($_GET){
+  //Boucle permettant l'affichage du toit
+    for($i=0; $i <= $_GET["hauteur"]; $i++){
+        // Boucle permettant l'espacement entres les "/"
+        for ($s = 0; $s <= ($_GET["hauteur"] - $i-1); $s++){
+            echo "&nbsp;";
+        }
+        echo "/";
+
+        // Boucle permettant l'affichage des underscores du toit "_"
+        for ($u = 1; $u <= ($i*2) ; $u++){
+            echo "_";
+        }
+        echo "\<br/>";
+    }
+  }
+
+  // Boucle permettant l'affichage du bas de la maison
+  for($i=1; $i <= $_GET["hauteur"]; $i++){
+    echo "|";
+    if ($i == $_GET["hauteur"]){
+        // Boucle pour le sol de la maison
+        for ($j=1; $j <= ($_GET["largeur"]); $j++){
+            echo "_";
+        }
+    }
+    else{
+        // Boucle d'espacement des murs
+        for ($j=1; $j <= ($_GET["largeur"]); $j++){
+            echo "&nbsp;";
+        }
+    }
+
+    echo "|<br/>";
+}
+
+?>
+</pre>
