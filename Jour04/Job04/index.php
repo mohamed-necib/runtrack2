ff7e69c7a7ce1c9198f11f22f5766ee042333b4f
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2022-11-17 14:23:37 +0100

    Ajout Job04 - Mise en place d un algorythme permettant d afficher dans un tableau HTML l ensemble des arguments contenu dans $_POST et les valeurs associés, deux colonnes: argument, valeur

---
+++ Jour04/Job04/index.php
@@ -0,0 +1,52 @@
+<!-- Développer un algorithme qui affiche dans un tableau HTML <table> l’ensemble des
+arguments $_ POST et les valeurs associées. Il doit y avoir deux colonnes : argument et
+valeur.
+Tips :
+Pour tester votre code, créez un formulaire html <form> de type POST avec différents
+champs <input> de type “text” et un <input> de type “submit” pour l’envoi. -->
+
+<form action="index.php" method="post" name="formulaire">
+<br/><input type="text" placeholder="Prenom" name="prenom" value="">
+  
+<input type="text" placeholder="Nom" name="nom" value="">
+  
+<input type="text" placeholder="Age" name="age" value="" >
+  
+<input type="submit" value="Envoyer">
+
+</form>
+
+
+<?php
+
+//
+
+if($_POST){
+  echo "
+  <style type='text/css'>
+.tftable {font-size:12px;color:#333333;width:100%;border-width: 1px;border-color: #ebab3a;border-collapse: collapse;}
+.tftable th {font-size:12px;background-color:#e6983b;border-width: 1px;padding: 8px;border-style: solid;border-color: #ebab3a;text-align:left;}
+.tftable tr {background-color:#f0c169;}
+.tftable td {font-size:12px;border-width: 1px;padding: 8px;border-style: solid;border-color: #ebab3a;}
+.tftable tr:hover {background-color:#ffffff;}
+</style>
+  <table class='tftable' border='1'>
+  <thead>
+      <tr>
+          <td>Argument</td>
+          <td>Valeur</td>
+      </tr>
+  </thead>
+  <tbody>";
+
+  foreach($_POST as $key => $value){
+      echo "<tr>
+              <td>$key</td>
+              <td>$value</td>
+          </tr>";
+  }
+  echo "</tbody></table>";
+}
+
+
+?>
