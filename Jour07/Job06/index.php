e8987f3a0cfa08894f99cc2ea2974ffeb57a27ee
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2022-11-22 15:47:19 +0100

    Job06 (Rework) - Création d'une fonction qui prend en paramètre une chaîne de caractères nommée '', on crée donc un tableau associatif dans lequel on itère pour comparer les lettres entrée et les remplacers par la leetSpeak qui correspond

--- Jour07/Job06/index.php
+++ Jour07/Job06/index.php
@@ -13,41 +13,70 @@ Cela est valable que l’on crie ou non (majuscules et minuscules). -->
 
 <?php
 
-function leetSpeak($str){
-    
-  for ($i=0; isset($str[$i]) ; $i++) { 
-    switch($str[$i]){
-      case "a":
-      case "A":
-        $str[$i] = "4";
-        break;
-      case "b":
-      case "B":
-        $str[$i] = "8";
-        break;
-      case "e":
-      case "E":
-        $str[$i] = "3";
-        break;
-      case "g":
-      case "G":
-        $str[$i] = "6";
-        break;
-      case "l":
-      case "L":
-        $str[$i] = "1";
-        break;
-      case "s":
-      case "S":
-        $str[$i] = "5";
-        break;
-      case "t":
-      case "T":
-        $str[$i] = "7";
-        break;
+// function leetSpeak($str){
+
+//   for ($i=0; isset($str[$i]) ; $i++) { 
+//     switch($str[$i]){
+//       case "a":
+//       case "A":
+//         $str[$i] = "4";
+//         break;
+//       case "b":
+//       case "B":
+//         $str[$i] = "8";
+//         break;
+//       case "e":
+//       case "E":
+//         $str[$i] = "3";
+//         break;
+//       case "g":
+//       case "G":
+//         $str[$i] = "6";
+//         break;
+//       case "l":
+//       case "L":
+//         $str[$i] = "1";
+//         break;
+//       case "s":
+//       case "S":
+//         $str[$i] = "5";
+//         break;
+//       case "t":
+//       case "T":
+//         $str[$i] = "7";
+//         break;
+//     }
+//   }
+//   return $str;
+// }
+
+// echo leetSpeak("Ableton");
+
+function leetSpeak($str)
+{
+
+  $array = [
+    '4' => ['a', 'A'],
+    '8' => ['b', 'B'],
+    '3' => ['e', 'E'],
+    '6' => ['g', 'G'],
+    '1' => ['l', 'L'],
+    '5' => ['s', 'S'],
+    '7' => ['t', 'T']
+  ];
+
+  for ($i = 0; isset($str[$i]); $i++) {
+
+    foreach ($array as $item => $char) {
+
+      if ($str[$i] === $char[0] || $str[$i] === $char[1]) {
+        $str[$i] = $item;
+      }
     }
   }
   return $str;
 }
 
-echo leetSpeak("Ableton");
+
+
+echo leetSpeak('Ablation');
