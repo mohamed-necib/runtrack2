9b292c00fc88a07b64117e23cc54ea1c023dc1ee
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2022-11-23 11:36:36 +0100

    Job01 - Création d'une variable de session qui à chaque fois que l'on se connecte, incrémente de 1 + création d'un bouton pour reset notre session

---
+++ Jour08/Job01/index.php
@@ -0,0 +1,42 @@
+<!-- Créez une variable de session nommée “nbvisites”. A chaque fois que la page est
+visitée, ajoutez 1. Afficher le contenu de cette variable.
+Ajoutez un bouton nommé “reset” qui permet de réinitialiser ce compteur. -->
+
+<?php
+
+session_start();
+
+
+if (isset($_POST['reset'])){
+  $_SESSION['nbvisites'] = 0;
+
+}
+
+if (isset($_SESSION['nbvisites'])) {
+  $_SESSION['nbvisites']++;
+} else {
+  $_SESSION['nbvisites'] = 1;
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Compteur</title>
+</head>
+
+<body>
+  <h1>Vous vous êtes connecté <?php echo $_SESSION['nbvisites'] ?> fois</h1>
+  <form method="post">
+        <input type="submit" name="reset"
+                class="button" value="reset" />
+</body>
+
+</html>
