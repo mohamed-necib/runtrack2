3b59458e6d01e59504f55b022628f5a14a54777d
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2022-11-25 13:12:17 +0100

    Jour10-Job01- Connexion à la base de données à l'aide d'une requete SQL et récupération de l'ensemble des informations contenues dans notre table étudiants. Affichage du resultat de la requete dans un tableau html

---
+++ Jour10/Job01/index.php
@@ -0,0 +1,96 @@
+<!-- En utilisant php et mysqli, connectez-vous à la base de données “jour09”. A l’aide d’une
+requête SQL, récupérez l’ensemble des informations de la table etudiants. Affichez le
+résultat de cette requête dans un tableau html. La première ligne de votre tableau html
+(thead) doit contenir le nom des champs. Les suivantes (tbody) doivent contenir les
+données présentes dans votre base de données. -->
+
+<?php
+
+$mysqli = new mysqli("localhost", "root", "root", "jour09");
+
+$request = $mysqli->query("SELECT * FROM etudiants");
+
+$result_fetch_all = $request->fetch_all();
+
+
+//var_dump($result_fetch_all);
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tableau</title>
+</head>
+
+<body>
+  <style type="text/css">
+    .tftable {
+      font-size: 12px;
+      color: #333333;
+      width: 100%;
+      border-width: 1px;
+      border-color: #ebab3a;
+      border-collapse: collapse;
+    }
+
+    .tftable th {
+      font-size: 12px;
+      background-color: #e6983b;
+      border-width: 1px;
+      padding: 8px;
+      border-style: solid;
+      border-color: #ebab3a;
+      text-align: left;
+    }
+
+    .tftable tr {
+      background-color: #f0c169;
+    }
+
+    .tftable td {
+      font-size: 12px;
+      border-width: 1px;
+      padding: 8px;
+      border-style: solid;
+      border-color: #ebab3a;
+    }
+
+    .tftable tr:hover {
+      background-color: #ffffff;
+    }
+  </style>
+
+  <table class="tftable" border="1">
+    <tr>
+      <th>ID</th>
+      <th>Prenom</th>
+      <th>Nom</th>
+      <th>Naissance</th>
+      <th>Sexe</th>
+      <th>Email</th>
+    </tr>
+      <?php
+      foreach($result_fetch_all as $ligne){
+        echo "<tr>";
+        foreach($ligne as $case){
+          echo "<td>" . $case . "</td>";
+        }
+        echo "</tr>";
+      }
+      ?>
+    
+    
+  </table>
+
+
+
+</body>
+
+</html>
